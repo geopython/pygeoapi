558fc7e8eaa6e99dee8be7e49edddb87049af072
Tom Kralidis <tomkralidis@gmail.com>
2018-03-27 21:26:21 -0400

    Revert "remove application level CORS (leave to server)"
    This reverts commit 8e931e644f86b40a74e407098abdef2ce559b494.

--- pygeoapi/flask_app.py
+++ pygeoapi/flask_app.py
@@ -34,6 +34,7 @@ import yaml
 import click
 
 from flask import Flask, make_response, request
+from flask_cors import CORS
 
 from pygeoapi import views
 from pygeoapi.config import settings
@@ -108,6 +109,9 @@ def serve(ctx, debug=False):
 
     if not settings['server']['pretty_print']:
         APP.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+    if settings['server']['cors']:
+        CORS(APP)
 
     setup_logger()
     # TODO: get scheme
