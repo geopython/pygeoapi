832402471bf659a3e473542c7d51f17bafae8130
Tom Kralidis <tomkralidis@gmail.com>
2018-04-02 18:18:37 -0400

    add travis setup (#20)

--- pygeoapi/provider/sqlite.py
+++ pygeoapi/provider/sqlite.py
@@ -25,7 +25,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
-#
+# =================================================================
 
 import sqlite3
 import logging
