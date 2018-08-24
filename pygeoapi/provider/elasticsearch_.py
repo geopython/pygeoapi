63364c20bfd32632739571ed9b87d6dcccf864e4
acapparelli <adam.capparelli@mail.utoronto.ca>
2018-08-24 09:05:45 -0400

    Added try...except for stop iteration error. (#62)

--- pygeoapi/provider/elasticsearch_.py
+++ pygeoapi/provider/elasticsearch_.py
@@ -189,10 +189,13 @@ class ElasticsearchProvider(BaseProvider):
                                    index=self.index_name)
                 results = {'hits': {'total': limit, 'hits': []}}
                 for i in range(startindex + limit):
-                    if i >= startindex:
-                        results['hits']['hits'].append(next(gen))
-                    else:
-                        next(gen)
+                    try:
+                        if i >= startindex:
+                            results['hits']['hits'].append(next(gen))
+                        else:
+                            next(gen)
+                    except StopIteration:
+                        break
             else:
                 results = self.es.search(index=self.index_name,
                                          from_=startindex, size=limit,
