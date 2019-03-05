2518d4b0a8d3a30fd9bedf6374e19167a825d17b
Tom Kralidis <tomkralidis@gmail.com>
2019-03-05 06:36:43 -0500

    add processing framework (#84)

---
+++ pygeoapi/process/hello_world.py
@@ -0,0 +1,96 @@
+# =================================================================
+#
+# Authors: Tom Kralidis <tomkralidis@gmail.com>
+#
+# Copyright (c) 2019 Tom Kralidis
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+import logging
+
+from pygeoapi.process.base import BaseProcessor
+
+LOGGER = logging.getLogger(__name__)
+
+PROCESS_METADATA = {
+    'version': '0.1.0',
+    'id': 'hello-world',
+    'title': 'Hello World process',
+    'description': 'Hello World process',
+    'keywords': ['hello world'],
+    'links': [{
+        'type': 'text/html',
+        'rel': 'canonical',
+        'title': 'information',
+        'href': 'https://example.org/process',
+        'hreflang': 'en-US'
+    }],
+    'inputs': [{
+        'id': 'name',
+        'title': 'name',
+        'input': {
+          'literalDataDomain': {
+            'dataType': 'string',
+            'valueDefinition': {
+              'anyValue': True
+            }
+          }
+        },
+        'minOccurs': 1,
+        'maxOccurs': 1
+    }],
+    'outputs': [{
+        'id': 'hello-world-response',
+        'title': 'output hello world',
+        'input': {
+            'formats': [{
+                'mimeType': 'application/json'
+            }]
+        }
+    }]
+}
+
+
+class HelloWorldProcessor(BaseProcessor):
+    """Hello World Processor"""
+
+    def __init__(self, provider_def):
+        """
+        Initialize object
+        :param provider_def: provider definition
+        :returns: pygeoapi.process.hello_world.HelloWorldProcessor
+        """
+
+        BaseProcessor.__init__(self, provider_def, PROCESS_METADATA)
+
+    def execute(self, data):
+        outputs = [{
+            'id': 'name',
+            'value': data['name']
+        }]
+
+        return outputs
+
+    def __repr__(self):
+        return '<HelloWorldProcessor> {}'.format(self.name)
