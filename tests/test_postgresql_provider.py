986ea503d4e57d4a6bd1c0ea3687cd32503d43d9
mbucknell <mbucknell@usgs.gov>
2019-11-25 09:16:52 -0600

    Put tests for column_mapping property in test_postgresql_provider and added assert for correct column property mapping.

--- tests/test_postgresql_provider.py
+++ tests/test_postgresql_provider.py
@@ -51,6 +51,8 @@ def config():
     }
 
 
+
+
 def test_query(config):
     """Testing query for a valid JSON object with geometry"""
 
@@ -104,5 +106,86 @@ def test_get(config):
     assert isinstance(result, dict)
     assert 'geometry' in result
     assert 'properties' in result
+    assert 'id' in result
+    assert 'Kanyosha' in result['properties']['name']
+
+@pytest.fixture()
+def col_prop_config():
+    return {
+        'name': 'PostgreSQL',
+        'data': {'host': '127.0.0.1',
+                 'dbname': 'test',
+                 'user': 'postgres',
+                 'password': 'postgres',
+                 'search_path': ['osm', 'public']
+                 },
+        'id_field': 'osm_id',
+        'column_property_mapping': {
+            'name': 'name',
+            'waterway': 'waterWay',
+            'covered': 'covered',
+            'width': 'width',
+            'depth': 'depth',
+            'z_index': 'zIndex'
+        },
+        'table': 'hotosm_bdi_waterways',
+        'geom_field': 'foo_geom'
+    }
+
+def test_query_with_col_prop(col_prop_config):
+    """Testing query for a valid JSON object with geometry"""
+
+    p = PostgreSQLProvider(col_prop_config)
+    feature_collection = p.query()
+    assert feature_collection.get('type', None) == 'FeatureCollection'
+    features = feature_collection.get('features', None)
+    assert features is not None
+    feature = features[0]
+    properties = feature.get('properties', None)
+    assert properties is not None
+    assert properties.get('zIndex', None) is not None
+    geometry = feature.get('geometry', None)
+    assert geometry is not None
+
+
+def test_query_with_col_prop_and_with_property_filter(col_prop_config):
+    """Test query  valid features when filtering by property"""
+    p = PostgreSQLProvider(col_prop_config)
+    feature_collection = p.query(properties=[("waterWay", "stream")])
+    features = feature_collection.get('features', None)
+    stream_features = list(
+        filter(lambda feature: feature['properties']['waterWay'] == 'stream',
+               features))
+    assert (len(features) == len(stream_features))
+
+    feature_collection = p.query()
+    features = feature_collection.get('features', None)
+    stream_features = list(
+        filter(lambda feature: feature['properties']['waterWay'] == 'stream',
+               features))
+    other_features = list(
+        filter(lambda feature: feature['properties']['waterWay'] != 'stream',
+               features))
+    assert (len(features) != len(stream_features))
+    assert (len(other_features) != 0)
+
+
+def test_query_with_col_prop_bbox(col_prop_config):
+    """Test query with a specified bounding box"""
+    psp = PostgreSQLProvider(col_prop_config)
+    boxed_feature_collection = psp.query(
+        bbox=[29.3373, -3.4099, 29.3761, -3.3924]
+    )
+    assert len(boxed_feature_collection['features']) == 5
+
+
+def test_get_with_col_prop(col_prop_config):
+    """Testing query for a specific object"""
+    p = PostgreSQLProvider(col_prop_config)
+    result = p.get(29701937)
+    assert isinstance(result, dict)
+    assert 'geometry' in result
+    assert 'properties' in result
+    assert 'zIndex' in result.get('properties')
     assert 'id' in result
     assert 'Kanyosha' in result['properties']['name']
