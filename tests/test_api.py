e02232c660a7c6fe0218e65e28ffe4a28cce72de
Tom Kralidis <tomkralidis@gmail.com>
2020-04-13 08:59:00 -0400

    remove now irrelevant

--- tests/test_api.py
+++ tests/test_api.py
@@ -605,15 +605,13 @@ def test_describe_processes(config, api_):
     assert len(process['outputTransmission']) == 1
     assert len(process['jobControlOptions']) == 1
 
-    api_.config['processes'] = {}
+    api_.config['resources'] = {}
 
     req_headers = make_req_headers()
     rsp_headers, code, response = api_.describe_processes(
         req_headers, {}, 'foo')
     processes = json.loads(response)
     assert len(processes['processes']) == 0
-
-    api_.config.pop('processes')
 
     req_headers = make_req_headers()
     rsp_headers, code, response = api_.describe_processes(
@@ -645,7 +643,7 @@ def test_execute_process(config, api_):
 
     assert response['outputs'][0]['value'] == 'test'
 
-    api_.config['processes'] = {}
+    api_.config['resources'] = {}
 
     req_headers = make_req_headers()
     rsp_headers, code, response = api_.execute_process(req_headers, {},
@@ -653,8 +651,6 @@ def test_execute_process(config, api_):
                                                        'hello-world')
     response = json.loads(response)
     assert response['code'] == 'NotFound'
-
-    api_.config.pop('processes')
 
     req_headers = make_req_headers()
     rsp_headers, code, response = api_.execute_process(req_headers, {},
