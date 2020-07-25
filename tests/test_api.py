e989cdf8e2bbbc8a5913962756c90ca935ad7637
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2020-07-25 12:56:25 +0200

    Fix test for conformance classes

--- tests/test_api.py
+++ tests/test_api.py
@@ -169,7 +169,7 @@ def test_conformance(config, api_):
 
     assert isinstance(root, dict)
     assert 'conformsTo' in root
-    assert len(root['conformsTo']) == 4
+    assert len(root['conformsTo']) == 6
 
     rsp_headers, code, response = api_.conformance(req_headers, {'f': 'foo'})
     assert code == 400
