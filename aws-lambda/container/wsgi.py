def94dd364429354ab88c58bad5b7d0dcfc3a178
Tom Kralidis <tomkralidis@gmail.com>
2021-09-19 19:14:04 -0400

    fix flake8 and add MIME types to lambda setup

--- aws-lambda/container/wsgi.py
+++ aws-lambda/container/wsgi.py
@@ -1,9 +1,10 @@
 import os
 
+from pygeoapi.flask_app import APP
+
 os.environ['PYGEOAPI_CONFIG'] = 'pygeoapi-test-config.yml'
 os.environ['PYGEOAPI_OPENAPI'] = 'pygeoapi-test-openapi.yml'
 
-from pygeoapi.flask_app import APP
 
 if __name__ == "__main__":
     APP.run()
