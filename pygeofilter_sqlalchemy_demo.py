c65048cdc82ca121f1140932ed394f8311ba64fa
Colin Blackburn <colb@bgs.ac.uk>
2022-07-13 16:33:05 +0100

    Use function for cql query

--- pygeofilter_sqlalchemy_demo.py
+++ pygeofilter_sqlalchemy_demo.py
@@ -39,40 +39,11 @@ OFFSET = 20
 LIMIT = 20
 SORTBY = [{'property': 'waterway', 'order': '-'},
           {'property': 'osm_id', 'order': '+'}]
-# Very later
 RESULTTYPE = 'results' # or 'hits' for count only
+# Very later
 SELECT_PROPERTIES = [] # Subset of columns
 SKIP_GEOMETRY = False
 
-# Done in the API
-ast = parse(CQL_QUERY)
-
-# Done in the provider
-
-# Connect to database and read tables
-connection_string = 'postgresql://postgres:postgres@localhost:5432/test'
-engine = create_engine(connection_string)
-metadata = MetaData(engine)
-metadata.reflect(schema=SCHEMAS[0], views=True)
-
-# Create SQLAlchemy model from reflected table
-# It is necessary to add the primary key constraint because SQLAlchemy
-# requires it to reflect the table, but a view in a PostgreSQL database does
-# not have a primary key defined.
-sqlalchemy_table_def = metadata.tables[f'{SCHEMAS[0]}.{TABLE}']
-sqlalchemy_table_def.append_constraint(PrimaryKeyConstraint(ID_FIELD))
-Base = automap_base(metadata=metadata)
-Base.prepare()
-TableModel = getattr(Base.classes, TABLE)
-
-# Prepare CQL requirements
-field_mapping = {column_name: getattr(TableModel, column_name)
-                 for column_name in TableModel.__table__.columns.keys()}
-filters = to_filter(ast, field_mapping)
-
-# Create session to run a query
-Session = sessionmaker(bind=engine)
-session = Session()
 
 # Create a list of order_by clauses
 def get_order_by_clauses(sort_by, table_model):
@@ -83,12 +54,54 @@ def get_order_by_clauses(sort_by, table_model):
         clauses.append(order_function(model_column))
     return clauses
 
-order_by_clauses = get_order_by_clauses(SORTBY, TableModel)
 
-print(f"Querying {TABLE}: {CQL_QUERY}")
-q = session.query(TableModel).filter(filters).order_by(*order_by_clauses).offset(OFFSET).limit(LIMIT)
-for row in q:
-    row_dict = row.__dict__
-    row_dict.pop('_sa_instance_state')  # Internal SQLAlchemy metadata
-    geom = row_dict.pop(GEOM_FIELD)
-    print(row_dict['osm_id'], row_dict['waterway'])
+def query_cql(engine, offset=0, limit=10, resulttype='results',
+              bbox=[], sortby=[], select_properties=[], skip_geometry=False,
+              cql_ast=None, **kwargs):
+
+    metadata = MetaData(engine)
+    metadata.reflect(schema=SCHEMAS[0], views=True)
+
+    # Create SQLAlchemy model from reflected table
+    # It is necessary to add the primary key constraint because SQLAlchemy
+    # requires it to reflect the table, but a view in a PostgreSQL database does
+    # not have a primary key defined.
+    sqlalchemy_table_def = metadata.tables[f'{SCHEMAS[0]}.{TABLE}']
+    sqlalchemy_table_def.append_constraint(PrimaryKeyConstraint(ID_FIELD))
+    Base = automap_base(metadata=metadata)
+    Base.prepare()
+    TableModel = getattr(Base.classes, TABLE)
+
+    # Prepare CQL requirements
+    field_mapping = {column_name: getattr(TableModel, column_name)
+                     for column_name in TableModel.__table__.columns.keys()}
+    filters = to_filter(ast, field_mapping)
+
+    # Create session to run a query
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    order_by_clauses = get_order_by_clauses(sortby, TableModel)
+
+    print(f"Querying {TABLE}: {CQL_QUERY}")
+    q = session.query(TableModel).filter(filters).order_by(*order_by_clauses).offset(offset).limit(limit)
+
+    result = []
+    for row in q:
+        row_dict = row.__dict__
+        row_dict.pop('_sa_instance_state')  # Internal SQLAlchemy metadata
+        result.append(row_dict)
+
+    return result
+
+
+if __name__ == '__main__':
+    # Done in the API
+    ast = parse(CQL_QUERY)
+    # Connect to database and read tables
+    connection_string = 'postgresql://postgres:postgres@localhost:5432/test'
+    engine = create_engine(connection_string)
+    result = query_cql(engine, offset=OFFSET, limit=LIMIT, sortby=SORTBY, cql_ast=ast)
+
+    pprint(result)
