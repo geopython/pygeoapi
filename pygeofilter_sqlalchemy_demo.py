652dad2994e1716f6e9705eddcfe9fb682b2b0b9
Colin Blackburn <colb@bgs.ac.uk>
2022-07-13 11:47:02 +0100

    Make script generic

--- pygeofilter_sqlalchemy_demo.py
+++ pygeofilter_sqlalchemy_demo.py
@@ -19,8 +19,8 @@ from geoalchemy2 import Geometry  # noqa - this isn't used explicitly but is nee
 from pygeofilter.parsers.ecql import parse
 from pygeofilter.backends.sqlalchemy.evaluate import to_filter
 
-
-TABLE ='published.ql_sen_sensor_location'
+SCHEMAS = ['published', 'public']
+TABLE = 'ql_sen_sensor_location'
 ID_COLUMN = 'sensor_loc_id'
 CQL_QUERY = "sensor_loc_id BETWEEN 52970 AND 100000" # Add to query method
 # Later
@@ -40,32 +40,32 @@ ast = parse(CQL_QUERY)
 connection_string = os.environ['CONN_STR']
 engine = create_engine(connection_string)
 metadata = MetaData(engine)
-metadata.reflect(schema='published', views=True)
+metadata.reflect(schema=SCHEMAS[0], views=True)
 
 # Create SQLAlchemy model from reflected table
 # It is necessary to add the primary key constraint because SQLAlchemy
 # requires it to reflect the table, but a view in a PostgreSQL database does
 # not have a primary key defined.
-sensor_location = metadata.tables[TABLE]
-sensor_location.append_constraint(PrimaryKeyConstraint(ID_COLUMN))
+sqlalchemy_table_def = metadata.tables[f'{SCHEMAS[0]}.{TABLE}']
+sqlalchemy_table_def.append_constraint(PrimaryKeyConstraint(ID_COLUMN))
 Base = automap_base(metadata=metadata)
 Base.prepare()
 
 # TODO: make this generic
-SensorLocation = Base.classes.ql_sen_sensor_location
+TableModel = getattr(Base.classes, TABLE)
 
 # Create session to run a query
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # Prepare CQL requirements
-field_mapping = {column_name: getattr(SensorLocation, column_name)
-                 for column_name in SensorLocation.__table__.columns.keys()}
+field_mapping = {column_name: getattr(TableModel, column_name)
+                 for column_name in TableModel.__table__.columns.keys()}
 filters = to_filter(ast, field_mapping)
 
 # Run the query
 print(f"Querying {TABLE}: {CQL_QUERY}")
-q = session.query(SensorLocation).filter(filters)
+q = session.query(TableModel).filter(filters)
 for row in q:
     print(','.join(str(item) 
                    for item in (row.sensor_loc_id, row.x, row.y, row.site_trans)))
