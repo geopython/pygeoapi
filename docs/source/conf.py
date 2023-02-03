7b925f1bbb728bfe1b2ac19a1e6c4858152fddcc
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2023-02-03 14:36:50 +0200

    update release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ today_fmt = '%Y-%m-%d'
 # built documents.
 #
 # The short X.Y version.
-version = '0.14.dev0'
+version = '0.14.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
