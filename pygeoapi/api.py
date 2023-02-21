af312f115d4f794efdb3d297ee1ac7e456750674
Tom Kralidis <tomkralidis@gmail.com>
2023-02-21 16:43:32 -0500

    add server limit to EDR calls that support paging (#1153)

--- pygeoapi/api.py
+++ pygeoapi/api.py
@@ -3677,7 +3677,8 @@ class API:
             z=z,
             bbox=bbox,
             within=within,
-            within_units=within_units
+            within_units=within_units,
+            limit=int(self.config['server']['limit'])
         )
 
         try:
