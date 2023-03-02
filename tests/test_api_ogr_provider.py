ac07ba7c6b77eba3d64430ec197cc66633f63a75
Just van den Broecke <just@justobjects.nl>
2023-03-02 20:38:56 +0100

    #1128 fix flake8 - install GDAL in workflow main for OGR tests

--- tests/test_api_ogr_provider.py
+++ tests/test_api_ogr_provider.py
@@ -52,24 +52,56 @@ def api_(config):
 
 
 def test_get_collection_items_bbox_crs(config, api_):
+    CRS_BBOX_DICT = {
+        'http://www.opengis.net/def/crs/OGC/1.3/CRS84': '5.71484, 52.12122, 5.71486, 52.12123', # noqa
+        'http://www.opengis.net/def/crs/EPSG/0/4326': '52.12122, 5.71484, 52.12123, 5.71486', # noqa
+        'http://www.opengis.net/def/crs/EPSG/0/28992': '177430,	459268, 177440,	459278' # noqa
+    }
 
-    # bbox-crs full extent
-    req = mock_request({'bbox': '5.670670, 52.042700, 5.829110, 52.123700', 'bbox-crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'}) # noqa
-    rsp_headers, code, response = api_.get_collection_items(req, 'dutch_addresses_4326') # noqa
-    features = json.loads(response)['features']
+    COLLECTIONS = ['dutch_addresses_4326', 'dutch_addresses_28992']
+    for coll in COLLECTIONS:
+        # bbox-crs full extent
+        req = mock_request({'bbox': '5.670670, 52.042700, 5.829110, 52.123700', 'bbox-crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'}) # noqa
+        rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+        features = json.loads(response)['features']
 
-    assert len(features) == 10
+        assert len(features) == 10
 
-    # bbox-crs outside extent
-    req = mock_request({'bbox': '5, 51.9, 5.1, 52.0', 'bbox-crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'}) # noqa
-    rsp_headers, code, response = api_.get_collection_items(req, 'dutch_addresses_4326') # noqa
-    features = json.loads(response)['features']
+        # bbox-crs partial extent, 1 feature, request with multiple CRSs
+        for crs in CRS_BBOX_DICT:
+            req = mock_request({'bbox': CRS_BBOX_DICT[crs], 'bbox-crs': crs}) # noqa
+            rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+            features = json.loads(response)['features']
 
-    assert len(features) == 0
+            assert len(features) == 1
+            properties = features[0]['properties']
+            assert properties['straatnaam'] == 'Willinkhuizersteeg'
+            assert properties['huisnummer'] == '2'
 
-    # bbox-crs full extent - axis reversed CRS
-    req = mock_request({'bbox': '52.042700, 5.670670, 52.123700, 5.829110', 'bbox-crs': 'http://www.opengis.net/def/crs/EPSG/0/4326'}) # noqa
-    rsp_headers, code, response = api_.get_collection_items(req, 'dutch_addresses_4326') # noqa
-    features = json.loads(response)['features']
+        # bbox-crs outside extent
+        req = mock_request({'bbox': '5, 51.9, 5.1, 52.0', 'bbox-crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'}) # noqa
+        rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+        features = json.loads(response)['features']
 
-    assert len(features) == 10
+        assert len(features) == 0
+
+        # bbox-crs outside extent
+        req = mock_request({'bbox': '130000, 440000, 140000, 450000', 'bbox-crs': 'http://www.opengis.net/def/crs/EPSG/0/28992'}) # noqa
+        rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+        features = json.loads(response)['features']
+
+        assert len(features) == 0
+
+        # bbox-crs outside extent - axis reversed CRS
+        req = mock_request({'bbox': '51.9, 5, 52.0, 5.1', 'bbox-crs': 'http://www.opengis.net/def/crs/EPSG/0/4326'}) # noqa
+        rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+        features = json.loads(response)['features']
+
+        assert len(features) == 0
+
+        # bbox-crs full extent - axis reversed CRS
+        req = mock_request({'bbox': '52.042700, 5.670670, 52.123700, 5.829110', 'bbox-crs': 'http://www.opengis.net/def/crs/EPSG/0/4326'}) # noqa
+        rsp_headers, code, response = api_.get_collection_items(req, coll) # noqa
+        features = json.loads(response)['features']
+
+        assert len(features) == 10
