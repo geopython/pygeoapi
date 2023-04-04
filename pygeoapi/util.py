f3734ad19346fe0a2d264bb0b71c515c78125e80
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2023-04-04 18:27:19 +0100

    Added enum for requested process execution modes

--- pygeoapi/util.py
+++ pygeoapi/util.py
@@ -548,6 +548,11 @@ def get_provider_default(providers: list) -> dict:
     return default
 
 
+class RequestedProcessExecutionMode(Enum):
+    wait = "wait"
+    respond_async = "respond-async"
+
+
 class JobStatus(Enum):
     """
     Enum for the job status options specified in the WPS 2.0 specification
