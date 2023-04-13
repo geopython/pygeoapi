0596e391c2d5d22e9ccec069f162355233c58f44
Mathieu Tachon <Mathieu.Tachon@kartverket.no>
2023-04-13 10:27:28 +0200

    remove blank line

--- pygeoapi/api.py
+++ pygeoapi/api.py
@@ -75,7 +75,6 @@ from pygeoapi.provider.tile import (ProviderTileNotFoundError,
                                     ProviderTileQueryError,
                                     ProviderTilesetIdNotFoundError)
 from pygeoapi.models.cql import CQLModel
-
 from pygeoapi.util import (dategetter, DATETIME_FORMAT, UrlPrefetcher,
                            filter_dict_by_key_value, get_provider_by_type,
                            get_provider_default, get_typed_value, JobStatus,
