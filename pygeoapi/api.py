70c59bcfc6e707de8d4f294e4836113fe7c73c96
IngenieroGeomatico <84268457+IngenieroGeomatico@users.noreply.github.com>
2023-04-25 19:36:39 +0200

    🔧 if conditional is added to the collections section (#1226)
    * :wrench: if conditional is added to the collections section
    
    * :wrench: if conditional is added to the collections section
    
    * :arrow_up: add collection key in data

--- pygeoapi/api.py
+++ pygeoapi/api.py
@@ -764,6 +764,7 @@ class API:
 
             fcm['processes'] = False
             fcm['stac'] = False
+            fcm['collection'] = False
 
             if filter_dict_by_key_value(self.config['resources'],
                                         'type', 'process'):
@@ -772,6 +773,10 @@ class API:
             if filter_dict_by_key_value(self.config['resources'],
                                         'type', 'stac-collection'):
                 fcm['stac'] = True
+
+            if filter_dict_by_key_value(self.config['resources'],
+                                        'type', 'collection'):
+                fcm['collection'] = True
 
             content = render_j2_template(self.tpl_config, 'landing_page.html',
                                          fcm, request.locale)
