d1c208dc2fb91c52a6bc6f74360019b7994ecaa5
Bernhard Mallinger <bernhard.mallinger@eox.at>
2023-07-05 15:19:41 +0200

    Add ogc api callback class to conformance
    https://docs.ogc.org/is/18-062r2/18-062r2.html#toc67

--- pygeoapi/api.py
+++ pygeoapi/api.py
@@ -174,6 +174,7 @@ CONFORMANCE = {
         'http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/core',
         'http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/json',
         'http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/oas30'
+        'http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/callback',
     ],
     'edr': [
         'http://www.opengis.net/spec/ogcapi-edr-1/1.0/conf/core'
