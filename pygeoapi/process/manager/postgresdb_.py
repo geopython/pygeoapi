f62029e4592fdf235df37632172d55a02c957989
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2023-08-16 12:12:49 -0400

    Manager base now supporting a queue before launching processes. The queue can have a maximum size total and indicate a maximum of concurrent processes that should be allowed to execute. The queue is configurable via new parameters: max_concurrent and max_queue Manager base can now indicate if the result of the process should be stored in the database along with the job information (instead of stored in a local file on the server where pygeoapi runs). This is configurable via new parameter: result_in_db Manager base can now indicate if the internal error message should be stored in the database along with the job information (instead of printed out in console). This is configurable via new parameter: internal_error_in_db Manager base now returns the job id when running asynchronously (instead of returning nothing) Manager base has slightly different message indicators to indicate a job progression Process base can now be made aware that's it's running as part of a manager and update the manager progression level as part of the process execution
    Brand new PostgresDB manager (in addition to the TinyDB, MongoDB managers)
    New Extract process which enables to perform an extraction on multiple collections, of type feature or rasterio, given a list of collections and a clipping geometry

---
+++ pygeoapi/process/manager/postgresdb_.py
@@ -0,0 +1,347 @@
+# =================================================================
+#
+# Authors: Alexandre Roy <alexandre.roy@fujitsu.com>
+#
+# Copyright (c) 2023 Alexandre Roy
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+
+from contextlib import contextmanager
+import json, logging
+from pathlib import Path
+from typing import Any, Tuple
+
+import psycopg2
+import psycopg2.extras
+from psycopg2 import sql
+from pygeoapi import api_aws
+
+from pygeoapi.process.base import (
+    JobNotFoundError,
+    JobResultNotFoundError,
+)
+from pygeoapi.process.manager.base import BaseManager
+from pygeoapi.util import JobStatus
+
+LOGGER = logging.getLogger(__name__)
+
+
+class PostgresDBManager(BaseManager):
+    """TinyDB Manager"""
+
+    def __init__(self, manager_def: dict):
+        """
+        Initialize object
+
+        :param manager_def: manager definition
+
+        :returns: `pygeoapi.process.manager.base.BaseManager`
+        """
+
+        super().__init__(manager_def)
+        self.is_async = True
+        self.host = manager_def['connection']['host']
+        self.port = manager_def['connection']['port']
+        self.dbname = manager_def['connection']['dbname']
+        self.user = manager_def['connection']['user']
+        self.password = manager_def['connection']['password']
+
+        # Table specs
+        self.table_jobs = {
+            'table_name': "jobs",
+            'field_identifier': "identifier",
+            'field_process_id': "process_id",
+            'field_status': "status",
+            'field_progress': "progress",
+            'field_date_started': "job_start_datetime",
+            'field_date_ended': "job_end_datetime",
+            'field_location': "location",
+            'field_mimetype': "mimetype",
+            'field_message': "message",
+            'field_result': "result"
+        }
+
+    def open_conn(self):
+        """
+        Returns a connection to the database
+        """
+
+        return psycopg2.connect(host=self.host, port=self.port, dbname=self.dbname, user=self.user, password=self.password, sslmode="allow")
+
+    def destroy(self) -> bool:
+        """
+        Destroy manager
+
+        :returns: `bool` status of result
+        """
+
+        with self.open_conn() as conn:
+            # Open a cursor
+            with conn.cursor() as cur:
+                str_query = "DELETE FROM {table}"
+
+                # Query in the database
+                query = sql.SQL(str_query).format(
+                    table=sql.Identifier(self.dbname, self.table_jobs['table_name'])
+                )
+
+                # Execute cursor
+                cur.execute(query)
+            conn.commit()
+        return True
+
+    def get_jobs(self, status: JobStatus = None) -> list:
+        """
+        Get jobs
+
+        :param status: job status (accepted, running, successful,
+                       failed, results) (default is all)
+
+        :returns: 'list` of jobs (identifier, status, process identifier)
+        """
+
+        with self.open_conn() as conn:
+            # Open a cursor
+            with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+                if status:
+                    str_query = "SELECT * FROM {table} WHERE {field_status} = %s ORDER BY {field_order} DESC"
+
+                    # Query in the database
+                    query = sql.SQL(str_query).format(
+                        table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                        field_order=sql.Identifier(self.table_jobs['field_date_started']))
+
+                    # Execute cursor and fetch
+                    cur.execute(query, (status,))
+
+                else:
+                    str_query = "SELECT * FROM {table} ORDER BY {field_order} DESC"
+
+                    # Query in the database
+                    query = sql.SQL(str_query).format(
+                        table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                        field_order=sql.Identifier(self.table_jobs['field_date_started']))
+
+                    # Execute cursor and fetch
+                    cur.execute(query)
+
+                # Fetch all
+                return cur.fetchall()
+
+    def add_job(self, job_metadata: dict) -> str:
+        """
+        Add a job
+
+        :param job_metadata: `dict` of job metadata
+
+        :returns: identifier of added job
+        """
+
+        with self.open_conn() as conn:
+            # Open a cursor
+            with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+                str_query = """INSERT INTO {table} ({field_identifier},
+                                                    {field_process_id},
+                                                    {field_status},
+                                                    {field_progress},
+                                                    {field_job_start_datetime},
+                                                    {field_location},
+                                                    {field_mimetype},
+                                                    {field_message})
+                                             VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+                            """
+
+                # Query in the database
+                query = sql.SQL(str_query).format(
+                    table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                    field_identifier=sql.Identifier(self.table_jobs['field_identifier']),
+                    field_process_id=sql.Identifier(self.table_jobs['field_process_id']),
+                    field_status=sql.Identifier(self.table_jobs['field_status']),
+                    field_progress=sql.Identifier(self.table_jobs['field_progress']),
+                    field_job_start_datetime=sql.Identifier(self.table_jobs['field_date_started']),
+                    field_location=sql.Identifier(self.table_jobs['field_location']),
+                    field_mimetype=sql.Identifier(self.table_jobs['field_mimetype']),
+                    field_message=sql.Identifier(self.table_jobs['field_message']))
+
+                # Execute cursor and fetch
+                cur.execute(query, (job_metadata[self.table_jobs['field_identifier']],
+                                    job_metadata[self.table_jobs['field_process_id']],
+                                    job_metadata[self.table_jobs['field_status']],
+                                    job_metadata[self.table_jobs['field_progress']],
+                                    job_metadata[self.table_jobs['field_date_started']],
+                                    job_metadata[self.table_jobs['field_location']],
+                                    job_metadata[self.table_jobs['field_mimetype']],
+                                    job_metadata[self.table_jobs['field_message']]))
+            conn.commit()
+            return job_metadata['identifier']
+
+    def update_job(self, job_id: str, update_dict: dict) -> bool:
+        """
+        Updates a job
+
+        :param job_id: job identifier
+        :param update_dict: `dict` of property updates
+
+        :returns: `bool` of status result
+        """
+
+        with self.open_conn() as conn:
+            # Open a cursor
+            with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+                str_query = "UPDATE {table} SET "
+                i = 1
+                values = []
+                for k in update_dict:
+                    str_query = str_query + f" {k} = %s,"
+                    values.append(update_dict[k])
+                    i = i+1
+                str_query = str_query[:-1]
+                str_query = str_query + " WHERE {field_identifier}=%s"
+                values.append(job_id)
+
+                # Query in the database
+                query = sql.SQL(str_query).format(
+                    table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                    field_identifier=sql.Identifier(self.table_jobs['field_identifier']))
+
+                # Execute cursor and fetch
+                cur.execute(query, values)
+            conn.commit()
+            return True
+
+    def delete_job(self, job_id: str) -> bool:
+        """
+        Deletes a job
+
+        :param job_id: job identifier
+
+        :raises: JobNotFoundError: if the job_id does not correspond to a
+            known job
+        :return `bool` of status result
+        """
+        # delete result file if present
+        job_result = self.get_job(job_id)
+
+        if job_result:
+            location = job_result[self.table_jobs['field_location']]
+
+            with self.open_conn() as conn:
+                # Open a cursor
+                with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+                    str_query = "DELETE FROM {table} WHERE {field_identifier}=%s"
+                    print(str_query)
+
+                    # Query in the database
+                    query = sql.SQL(str_query).format(
+                        table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                        field_identifier=sql.Identifier(self.table_jobs['field_identifier']))
+
+                    # Execute cursor and fetch
+                    cur.execute(query, (job_id,))
+                conn.commit()
+                return True
+        else:
+             raise JobNotFoundError()
+
+    def get_job(self, job_id: str) -> dict:
+        """
+        Get a single job
+
+        :param job_id: job identifier
+
+        :raises: JobNotFoundError: if the job_id does not correspond to a
+            known job
+        :returns: `dict`  # `pygeoapi.process.manager.Job`
+        """
+
+        with self.open_conn() as conn:
+            # Open a cursor
+            with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+                str_query = "SELECT * FROM {table} WHERE {field_identifier}=%s"
+
+                # Query in the database
+                query = sql.SQL(str_query).format(
+                    table=sql.Identifier(self.dbname, self.table_jobs['table_name']),
+                    field_identifier=sql.Identifier(self.table_jobs['field_identifier']))
+
+                # Execute cursor and fetch
+                cur.execute(query, (job_id,))
+
+                # Fetch one
+                res = cur.fetchone()
+                if res:
+                    return res
+                raise JobNotFoundError()
+
+    def get_job_result(self, job_id: str) -> Tuple[str, Any]:
+        """
+        Get a job's status, and actual output of executing the process
+
+        :param job_id: job identifier
+
+        :raises: JobNotFoundError: if the job_id does not correspond to a
+            known job
+        :raises: JobResultNotFoundError: if the job-related result cannot
+            be returned
+        :returns: `tuple` of mimetype and raw output
+        """
+
+        job_result = self.get_job(job_id)
+
+        # If job was found
+        if job_result:
+            # Read the status
+            job_status = JobStatus[job_result[self.table_jobs['field_status']]]
+
+            # If job is complete
+            if job_status == JobStatus.successful:
+                # Read the mimetype
+                mimetype = job_result[self.table_jobs['field_mimetype']]
+
+                # If the job is in the database
+                if self.result_in_db:
+                    # Read the result
+                    result = json.loads(job_result[self.table_jobs['field_result']])
+                    return mimetype, result
+
+                elif location:
+                    # The result is in a file (default)
+                    location = Path(location)
+                    with location.open('r', encoding='utf-8') as filehandler:
+                        result = json.load(filehandler)
+                    return mimetype, result
+
+                else:
+                    LOGGER.warning(f'job {job_id!r} -  unknown result location')
+                    raise JobResultNotFoundError()
+
+            # Job is incomplete
+            return (None,)
+
+        else:
+            raise JobNotFoundError()
+
+    def __repr__(self):
+        return f'<PostgresDBManager> {self.name}'
