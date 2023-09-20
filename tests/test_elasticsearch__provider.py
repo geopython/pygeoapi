f8a6e2dcf72cd4d36e84645db68a44fd3339a45a
Mathieu Tachon <Mathieu.Tachon@kartverket.no>
2023-09-20 22:42:33 +0200

    Refactor code base to make it work with pydantic v2

--- tests/test_elasticsearch__provider.py
+++ tests/test_elasticsearch__provider.py
@@ -78,7 +78,7 @@ def between():
             "upper": 100000
         }
     }
-    return CQLModel.parse_obj(between_)
+    return CQLModel.model_validate(between_)
 
 
 @pytest.fixture()
@@ -89,7 +89,7 @@ def between_upper():
             "upper": 100000
         }
     }
-    return CQLModel.parse_obj(between_)
+    return CQLModel.model_validate(between_)
 
 
 @pytest.fixture()
@@ -100,7 +100,7 @@ def between_lower():
             "lower": 10000
         }
     }
-    return CQLModel.parse_obj(between_)
+    return CQLModel.model_validate(between_)
 
 
 @pytest.fixture()
@@ -111,7 +111,7 @@ def eq():
             "Admin-0 capital"
         ]
     }
-    return CQLModel.parse_obj(eq_)
+    return CQLModel.model_validate(eq_)
 
 
 @pytest.fixture()
@@ -135,7 +135,7 @@ def _and(eq, between):
             }
         ]
     }
-    return CQLModel.parse_obj(and_)
+    return CQLModel.model_validate(and_)
 
 
 @pytest.fixture()
@@ -155,7 +155,7 @@ def intersects():
             ]
         }
     ]}
-    return CQLModel.parse_obj(intersects)
+    return CQLModel.model_validate(intersects)
 
 
 def test_query(config):
