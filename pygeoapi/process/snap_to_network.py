f7e926801fa56a3033a302c00119188167891e7d
Merret <merret.buurman@igb-berlin.de>
2023-10-11 18:35:09 +0200

    Added snap_to_network tool, first iteration.

---
+++ pygeoapi/process/snap_to_network.py
@@ -0,0 +1,257 @@
+
+import logging
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+LOGGER = logging.getLogger(__name__)
+
+import argparse
+import subprocess
+import geojson
+import os
+
+
+'''
+
+Written by Merret on 2023-10-06 - 2023-10-11
+Just for testing purposes - licenses not checked yet!
+
+It does the same as snap_to_network.R,
+see https://github.com/glowabio/hydrographr/blob/HEAD/R/snap_to_network.R
+Which calls:
+https://github.com/glowabio/hydrographr/blob/a818de3e9165d108034fd342c2a85e0a3c7ec7ca/inst/sh/snap_to_network.sh
+Done to to understand what that one is doing exactly!
+
+Documentation:
+https://glowabio.github.io/hydrographr/reference/snap_to_network.html
+
+How to call this service?
+curl -X POST "http://localhost:5000/processes/snap-to-network/execution" -H "Content-Type: application/json" -d "{\"inputs\":{\"method\": \"distance\", \"accumulation\": 0.5, \"distance\": 500, \"coordinate_multipoint\":{\"coordinates\": [[-17.25355, -44.885825], [-13.763611, -43.595833]], \"type\": \"MultiPoint\"}}}" 
+
+How to add this service to an existing pygeoapi instance?
+* Add to plugins.py
+* Add to pygeoapi-config.yml
+* Add code into directory process (bash scripts need to be executable!)
+* Install any python dependencies into the pygeoapo virtualenv!
+
+'''
+
+
+
+#: Process metadata and description
+PROCESS_METADATA = {
+    'version': '0.0.1',
+    'id': 'snap-to-network',
+    'title': {'en': 'Snap to network'},
+    'description': {
+        'en': 'Trying to expose snap_to_network from hydrographr package as ogc process: '
+              'https://glowabio.github.io/hydrographr/reference/snap_to_network.html.'
+    },
+    'jobControlOptions': ['sync-execute', 'async-execute'],
+    'keywords': ['hydrographr', 'example', 'echo'],
+    'links': [{
+        'type': 'text/html',
+        'rel': 'about',
+        'title': 'information',
+        'href': 'https://example.org/process',
+        'hreflang': 'en-US'
+    }],
+    'inputs': {
+        'coordinate_multipoint': {
+            'title': 'Coordinate Multipoint',
+            'description': 'Dunno exactly.',
+            'schema': {
+                'type': 'object',
+                'contentMediaType': 'application/json'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,  # TODO how to use?
+            'keywords': ['coordinates']
+        },
+        'method': {
+            'title': 'Method',
+            'description': '"distance", "accumulation", or "both". Defines if the points are snapped using the distance or flow accumulation.',
+            'schema': {'type': 'string'},
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,  # TODO how to use?
+            'keywords': ['snapping']
+        },
+        'distance': {
+            'title': 'Distance',
+            'description': 'Maximum radius in map pixels. The points will be snapped to the next stream within this radius.',
+            'schema': {'type': 'string'},
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,  # TODO how to use?
+            'keywords': ['bla']
+        },
+        'accumulation': {
+            'title': 'Accumulation',
+            'description': 'Minimum flow accumulation. Points will be snapped to the next stream with a flow accumulation equal or higher than the given value.',
+            'schema': {'type': 'string'},
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,  # TODO how to use?
+            'keywords': ['bla']
+        }
+    },
+    'outputs': {
+        'snapped_points': {
+            'title': 'Snapped Points',
+            'description': 'Resulting coordinates as multipoint.',
+            'schema': {
+                'type': 'object',
+                'contentMediaType': 'application/json'
+            }
+        }
+    },
+    'example': {
+        'inputs': {
+            'coordinate_multipoint': "{\"coordinates\": [[-17.25355, -44.885825], [-13.763611, -43.595833]], \"type\": \"MultiPoint\"}"
+        }
+    }
+}
+
+class SnapToNetworkProcessor(BaseProcessor):
+    """Get Snap-to-Network Processor example"""
+
+    def __init__(self, processor_def):
+        """
+        Initialize object
+
+        :param processor_def: provider definition
+
+        :returns: pygeoapi.process.get_tide_id.SnapToNetworkProcessor
+        """
+
+        super().__init__(processor_def, PROCESS_METADATA)
+
+    def execute(self, data):
+        LOGGER.info('Starting "snap_to_network as ogc_service!"')
+
+        # Get input:
+        method = data.get('method')
+        distance = data.get('distance')
+        accumulation = data.get('accumulation')
+
+        # Check validity of argument:
+        if not method in ['distance', 'accumulation', 'both']:
+            LOGGER.error('Error: Wrong method: %s' % method)
+
+        # Already in geoJSON I think, no need to run geojson.loads()
+        multipoint = data.get('coordinate_multipoint')
+        LOGGER.debug('Input multipoint: %s (%s)' % (multipoint, type(multipoint)))
+
+        # Make a file out of the input geojson, as the bash file wants them as a file
+        # Otherwise, rewrite the bash file to python!
+        input_coord_file_path = '/tmp/__input_tuesday.txt'
+        col_name_lat = 'lat'
+        col_name_lon = 'lon'
+        col_name_id = 'dummy_unused' # or so! I don't think is is really used, is it? TODO check this third column business
+        self.make_file_from_geojson(multipoint, input_coord_file_path, col_name_lat, col_name_lon)
+
+        # Hardcoded paths on server:
+        # TODO: Do we have to cut them smaller for processing?
+        path_accumul_tif = '/home/mbuurman/work/testing_hydrographr/data/basin_481051/accumulation_481051.tif'
+        path_stream_tif  = '/home/mbuurman/work/testing_hydrographr/data/basin_481051/segment_481051.tif'
+        tmp_dir = '/tmp'
+        snap_tmp_path = '/tmp/__output_tuesday.txt' # intermediate result storage used by GRASS!
+
+        # Now call the tool:
+        snap_tmp_path = self.call_snap_to_network_sh(input_coord_file_path,
+            col_name_id, col_name_lon, col_name_lat,
+            path_stream_tif, path_accumul_tif,
+            method, distance, accumulation,
+            snap_tmp_path, tmp_dir)
+
+        # Now make geojson from the tool:
+        result_multipoint = self.csv_to_geojson(snap_tmp_path)
+        LOGGER.debug('________________________________')
+        LOGGER.info('Result multipoint: %s' % result_multipoint)
+        #LOGGER.info('Result multipoint: %s (dumped)' % geosjon.dumps(result_multipoint))
+
+        outputs = {
+            'id': 'snapped_points',
+            'value': result_multipoint
+        }
+
+        mimetype = 'application/json'
+        return mimetype, outputs
+
+    def __repr__(self):
+        return f'<SnapToNetworkProcessor> {self.name}'
+
+
+    def make_file_from_geojson(self, multipoint, input_coord_file_path, col_name_lat='lat', col_name_lon='lon', sep=','):
+
+        LOGGER.debug('Writing input coordinates from geojson into "%s"...' % input_coord_file_path)
+        with open(input_coord_file_path, 'w') as inputfile: # Overwrite previous input file!
+            inputfile.write('%s%s%s%s%s\n' % ("foo", sep, col_name_lon, sep, col_name_lat))
+            for coord_pair in multipoint['coordinates']:
+                lon = coord_pair[0]
+                lat = coord_pair[1]
+                coord_pair_str = '%s%s%s%s%s\n' % (99999, sep, lat, sep, lon) # comma separated
+                inputfile.write(coord_pair_str)
+
+        # DEBUG
+        # THis is what happens in R in this line:
+        # https://github.com/glowabio/hydrographr/blob/HEAD/R/snap_to_network.R#L193C3-L193C17
+        # They just export the coordinates, no other columns!
+        # But the input line in grass seems to expect more lines!
+        # Line 73 in snap_to_network.sh:
+        # https://github.com/glowabio/hydrographr/blob/24e350f1606e60a02594b4d655501ca68bc3e846/inst/sh/snap_to_network.sh#L73
+        # So I now added another dummy foo column in front.
+
+        return input_coord_file_path
+
+
+    def call_snap_to_network_sh(self, path_coord_file, id_col_name, lon_col_name, lat_col_name, path_stream_tif, path_accumul_tif, method, distance, accumulation, snap_tmp_path, tmp_dir):
+        
+        with open(snap_tmp_path, 'w') as myfile:
+            pass # To empty the file! #- can be done more elegant I'm sure!
+
+        LOGGER.debug('Now calling bash which calls grass/gdal...')
+        LOGGER.debug('Current directory: %s' % os.getcwd())
+        # bash_file = os.getcwd()+'/snap_to_network.sh'
+        # Does not exist: /home/mbuurman/work/project_AquaInfra/pygeoapi/test/pygeoapi/pygeoapi/snap_to_network.sh
+        # The real thing: /home/mbuurman/work/project_AquaInfra/pygeoapi/test/pygeoapi/pygeoapi/pygeoapi/process/snap_to_network.sh
+        bash_file = os.getcwd()+'/pygeoapi/process/snap_to_network.sh'
+        cmd =[bash_file, path_coord_file, id_col_name, lon_col_name, lat_col_name,
+              path_stream_tif, path_accumul_tif, method, str(distance), str(accumulation), snap_tmp_path, tmp_dir]
+        LOGGER.debug('GRASS command: %s' % cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdoutdata, stderrdata = p.communicate()
+        LOGGER.debug('Process exit code: %s' % p.returncode)
+        LOGGER.debug('Process output:\n____________\n%s\n_____(end of process stdout)_______' % stdoutdata.decode())
+        if len(stderrdata.decode()) > 0:
+            LOGGER.debug('Process errors:\n____________\n%s\n_____(end of process stderr)_______' % stderrdata.decode())
+        else:
+            LOGGER.debug('(Nothing written to stderr: "%s")' % stderrdata.decode())
+        return snap_tmp_path
+
+
+    def csv_to_geojson(self, snap_tmp_path):
+        # Get results by reading them from result file!
+        # This is how GeoJSON Multipoints look:
+        # "{\"coordinates\": [[-17.25355, -44.885825], [-13.763611, -43.595833]], \"type\": \"MultiPoint\"}"
+        result_multipoint = {"type": "MultiPoint", "coordinates":[]}
+        LOGGER.debug('Reading snapped points from GRASS result file "%s" and writing them into a GeoJSON Multipoint...' % snap_tmp_path)
+        firstline = True
+        with open(snap_tmp_path, 'r') as resultfile:
+            for line in resultfile:
+
+                # Ignore header line!
+                if firstline:
+                    firstline = False
+                    continue
+
+                line = line.strip()
+                LOGGER.debug('Found coordinate line: %s' % line) # TODO Understand why we get so many columns here!
+                splitted = line.split()
+
+                # Which columns?
+                coord1, coord2 = splitted[1], splitted[2]
+                LOGGER.debug('Extracted these coordinates %s, %s' % (coord1, coord2))
+                result_multipoint['coordinates'].append([coord2, coord1])
+        LOGGER.debug('Finished creating GeoJSON multipoint: %s' % geojson.dumps(result_multipoint))
+        return result_multipoint
---
+++ pygeoapi/process/snap_to_network.sh
@@ -0,0 +1,280 @@
+#! /bin/bash
+
+# Adapted version by Merret, 2023-10-10, 11:04
+
+##  file (e.g. txt or csv) that has been generated at the beginning
+##  of the R function, based on the data.frame table the user provided
+export DATA=$1
+
+## name of the unique id column
+export ID=$2
+
+## names of the lon and lat columns
+export LON=$3
+export LAT=$4
+
+## stream raster file (e.g. .tif file)
+export STR=$5
+
+## accumulation raster files
+export ACC=$6
+
+## What to calculate
+export METHOD=$7
+
+## radius distance
+export rdist=$8
+
+## accumulation threshold
+export acct=$9
+
+## Full path to output snap_points.txt file
+export SNAP=${10}
+
+## Temporary folder
+export DIR=${11}
+
+
+
+## Set random string
+export RAND_STRING=$(xxd -l 8 -c 32 -p < /dev/random)
+echo "MB: RAND_STRING: "+$RAND_STRING
+
+## save name of file without extension
+#b=$(echo $DAT | awk -F"." '{print $1}')
+
+# Note: Tmp output from R is already a .csv
+# if the file is not csv, add the comma and make it .csv
+#if [ "${DAT: -4}" != ".csv" ]
+#then
+#    cat  $DAT | tr -s '[:blank:]' ',' > ${b}.csv
+#    export DATC=$(echo ${b}.csv)
+#fi
+
+##  make the file a gpkg -- works only if EPSG is 4326 (WGS 84)
+# ogr2ogr -f "GPKG" -overwrite -nln ref_points -nlt POINT -a_srs EPSG:4326 \
+#     $DIR/ref_points_${RAND_STRING}.gpkg $DATA -oo X_POSSIBLE_NAMES=$LON \
+#     -oo Y_POSSIBLE_NAMES=$LAT -oo AUTODETECT_TYPE=YES
+
+# how many points originally (save as reference)
+# export op=$(ogrinfo -so -al $DIR/ref_points_${RAND_STRING}.gpkg \
+#     | awk '/Feature/ {print $3}')
+
+# how many points originally (save as reference)
+export op=$(tail -n +2 $DATA | wc -l)
+
+##  do the snapping in GRASS
+grass -f --gtext --tmp-location $STR  <<'EOF'
+
+# read stream raster file
+echo "1"
+r.in.gdal input=$STR output=stream
+echo "2"
+
+# read reference points
+# v.in.ogr --o input=$DIR/ref_points_${RAND_STRING}.gpkg layer=ref_points output=ref_points \
+#     type=point key=$ID
+
+v.in.ascii -z in=$DATA out=ref_points separator=comma \
+  cat=1 x=2 y=3 z=3 skip=1
+# DEBUG
+# grass v.in.ascii -z in="/tmp/__input_tuesday.txt" out=ref_points separator=comma cat=1 x=2 y=3 z=3 skip=1
+
+
+echo "3 in: "$DATA
+
+# if not then identify id of those left out
+
+if [ "$METHOD" = "distance" ]
+then
+    echo "4 distance"
+
+    r.stream.snap --o input=ref_points output=snap_points stream_rast=stream \
+        radius=$rdist
+
+    # are all the original points in there?
+    np=$(v.info snap_points | awk '/points:/{print $5}')
+
+    # if not, then identify the ids of the points left out (out of extention of
+    # stream raster file)
+    if [[ $op != $np  ]]
+    then
+        lo=($(comm -3 \
+            <(v.report -c map=ref_points option=coor | awk -F"|" '{print $1}') \
+            <(v.report -c map=snap_points option=coor | awk -F"|" '{print $1}')))
+
+        for i in ${lo[@]}
+        do
+            echo "$i,out-bbox,out-bbox,,NA" > $DIR/stream_ID_${RAND_STRING}_d_tmp.txt
+        done
+    fi
+
+    r.what --o -v map=stream points=snap_points separator=comma \
+        null_value=NA >> $DIR/stream_ID_${RAND_STRING}_d_tmp.txt
+
+    echo "lon_snap_dist lat_snap_dist occu_id" > $DIR/snap_coords_${RAND_STRING}_d.txt
+
+    if [[ "${#lo[@]}" -gt 0  ]]
+    then
+        for i in ${lo[@]}
+        do
+            echo "out-bbox out-bbox $i" >> $DIR/snap_coords_${RAND_STRING}_d.txt
+        done
+    fi
+
+    v.out.ascii -c input=snap_points separator=space | awk 'NR > 1' \
+        >> $DIR/snap_coords_${RAND_STRING}_d.txt
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+
+    paste -d" " \
+        <(sort -gk1n $DIR/coords_${RAND_STRING}.txt) \
+        <(sort -gk3n $DIR/snap_coords_${RAND_STRING}_d.txt) \
+        <(printf "%s\n" subc_id_snap_dist $(awk -F, '{print $1, $5}' $DIR/stream_ID_${RAND_STRING}_d_tmp.txt | sort -gk1n | awk '{print $2}'))  \
+        >  $DIR/final_tmp_${RAND_STRING}.txt
+
+    awk '{print $1, $2, $3, $4, $5, $7}' \
+        $DIR/final_tmp_${RAND_STRING}.txt \
+        > $SNAP
+
+    rm $DIR/snap_coords_${RAND_STRING}_d.txt $DIR/stream_ID_${RAND_STRING}_d_tmp.txt \
+       $DIR/final_tmp_${RAND_STRING}.txt
+fi
+#
+#
+if [ "$METHOD" = "accumulation" ]
+then
+    echo "4 accu"
+    r.in.gdal input=$ACC output=accum
+    r.stream.snap --o input=ref_points output=snap_points stream_rast=stream \
+        radius=$rdist accumulation=accum threshold=$acct
+
+    # are all the original points in there?
+    np=$(v.info snap_points | awk '/points:/{print $5}')
+
+    # if not, then identify the ids of the points left out (out of extention of
+    # stream raster file)
+    if [[ $op != $np  ]]
+    then
+        lo=($(comm -3 \
+            <(v.report -c map=ref_points option=coor | awk -F"|" '{print $1}') \
+            <(v.report -c map=snap_points option=coor | awk -F"|" '{print $1}')))
+
+        for i in ${lo[@]}
+        do
+            echo "$i,out-bbox,out-bbox,,NA" > $DIR/stream_ID_${RAND_STRING}_a_tmp.txt
+        done
+    fi
+
+    r.what --o -v map=stream points=snap_points separator=comma \
+        null_value=NA >> $DIR/stream_ID_${RAND_STRING}_a_tmp.txt
+
+    echo "lon_snap_accu lat_snap_accu occu_id" > $DIR/snap_coords_${RAND_STRING}_a.txt
+
+    if [[ "${#lo[@]}" -gt 0  ]]
+    then
+        for i in ${lo[@]}
+        do
+            echo "out-bbox out-bbox $i" >> $DIR/snap_coords_${RAND_STRING}_a.txt
+        done
+    fi
+
+    v.out.ascii -c input=snap_points separator=space | awk 'NR > 1' \
+        >> $DIR/snap_coords_${RAND_STRING}_a.txt
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+
+    paste -d" " \
+        <(sort -gk1n $DIR/coords_${RAND_STRING}.txt) \
+        <(sort -gk3n $DIR/snap_coords_${RAND_STRING}_a.txt) \
+        <(printf "%s\n" subc_id_snap_accu $(awk -F, '{print $1, $5}' $DIR/stream_ID_${RAND_STRING}_a_tmp.txt | sort -gk1n | awk '{print $2}'))  \
+        >  $DIR/final_tmp_${RAND_STRING}.txt
+
+    awk '{print $1, $2, $3, $4, $5, $7}' \
+        $DIR/final_tmp_${RAND_STRING}.txt \
+        > $SNAP
+
+    rm $DIR/snap_coords_${RAND_STRING}_*.txt $DIR/stream_ID_${RAND_STRING}_*.txt \
+        $DIR/final_tmp_${RAND_STRING}.txt
+fi
+
+
+if [ "$METHOD" = "both" ]
+then
+    r.stream.snap --o input=ref_points output=snap_points_d stream_rast=stream \
+        radius=$rdist
+
+    # are all the original points in there?
+    np=$(v.info snap_points_d | awk '/points:/{print $5}')
+
+    # if not, then identify the ids of the points left out (out of extention of
+    # stream raster file)
+    if [[ $op != $np  ]]
+    then
+        lo=($(comm -3 \
+            <(v.report -c map=ref_points option=coor | awk -F"|" '{print $1}') \
+            <(v.report -c map=snap_points_d option=coor | awk -F"|" '{print $1}')))
+
+        for i in ${lo[@]}
+        do
+            echo "$i,out-bbox,out-bbox,,NA" > $DIR/stream_ID_${RAND_STRING}_d_tmp.txt
+            echo "$i,out-bbox,out-bbox,,NA" > $DIR/stream_ID_${RAND_STRING}_a_tmp.txt
+        done
+    fi
+
+    r.what --o -v map=stream points=snap_points_d separator=comma \
+        null_value=NA >> $DIR/stream_ID_${RAND_STRING}_d_tmp.txt
+
+
+    r.in.gdal input=$ACC output=accum
+    r.stream.snap --o input=ref_points output=snap_points_a stream_rast=stream \
+        radius=$rdist accumulation=accum threshold=$acct
+
+    r.what --o -v map=stream points=snap_points_a separator=comma \
+        null_value=NA >> $DIR/stream_ID_${RAND_STRING}_a_tmp.txt
+
+
+    echo "lon_snap_dist lat_snap_dist occu_id" > $DIR/snap_coords_${RAND_STRING}_d.txt
+    echo "lon_snap_accu lat_snap_accu occu_id" > $DIR/snap_coords_${RAND_STRING}_a.txt
+
+    if [[ "${#lo[@]}" -gt 0  ]]
+    then
+        for i in ${lo[@]}
+        do
+            echo "out-bbox out-bbox $i" >> $DIR/snap_coords_${RAND_STRING}_d.txt
+            echo "out-bbox out-bbox $i" >> $DIR/snap_coords_${RAND_STRING}_a.txt
+        done
+    fi
+
+
+    v.out.ascii -c input=snap_points_d separator=space | awk 'NR > 1'  \
+        >> $DIR/snap_coords_${RAND_STRING}_d.txt
+
+
+    v.out.ascii -c input=snap_points_a separator=space | awk 'NR > 1' \
+        >> $DIR/snap_coords_${RAND_STRING}_a.txt
+
+
+    cat  $DATA | tr -s ',' ' ' > $DIR/coords_${RAND_STRING}.txt
+
+    paste -d" " \
+        <(sort -gk1n $DIR/coords_${RAND_STRING}.txt) \
+        <(sort -gk3n $DIR/snap_coords_${RAND_STRING}_d.txt) \
+        <(printf "%s\n" subc_id_snap_dist $(awk -F, '{print $1, $5}' $DIR/stream_ID_${RAND_STRING}_d_tmp.txt | sort -gk1n | awk '{print $2}'))  \
+        <(sort -gk3n $DIR/snap_coords_${RAND_STRING}_a.txt) \
+        <(printf "%s\n" subc_id_snap_accu $(awk -F, '{print $1, $5}' $DIR/stream_ID_${RAND_STRING}_a_tmp.txt | sort -gk1n | awk '{print $2}'))  \
+        >  $DIR/final_tmp_${RAND_STRING}.txt  #$SNAP
+
+    awk '{print $1, $2, $3, $4, $5, $7, $8, $9, $11}' \
+        $DIR/final_tmp_${RAND_STRING}.txt \
+        > $SNAP
+
+
+    rm $DIR/snap_coords_${RAND_STRING}_*.txt $DIR/stream_ID_${RAND_STRING}_*.txt \
+        $DIR/final_tmp_${RAND_STRING}.txt
+fi
+
+
+EOF
+
+# rm $DIR/ref_points_${RAND_STRING}.gpkg
