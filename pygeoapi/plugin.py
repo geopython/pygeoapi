bfd3cac20adac33165c164e43392d2fbee287c4f
Tom Kralidis <tomkralidis@gmail.com>
2023-11-01 19:35:14 -0400

    implement CSW facade (#1386)

--- pygeoapi/plugin.py
+++ pygeoapi/plugin.py
@@ -40,6 +40,7 @@ PLUGINS = {
     'provider': {
         'AzureBlobStorage': 'pygeoapi.provider.azure_.AzureBlobStorageProvider',  # noqa
         'CSV': 'pygeoapi.provider.csv_.CSVProvider',
+        'CSWFacade': 'pygeoapi.provider.csw_facade.CSWFacadeProvider',
         'Elasticsearch': 'pygeoapi.provider.elasticsearch_.ElasticsearchProvider',  # noqa
         'ElasticsearchCatalogue': 'pygeoapi.provider.elasticsearch_.ElasticsearchCatalogueProvider',  # noqa
         'ERDDAPTabledap': 'pygeoapi.provider.erddap.TabledapProvider',
