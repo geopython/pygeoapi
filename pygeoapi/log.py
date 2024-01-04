731a169d72874cdd6ff358bf850b790e624425f2
Bernhard Mallinger <bernhard.mallinger@eox.at>
2024-01-04 14:30:17 +0100

    Fix import order

--- pygeoapi/log.py
+++ pygeoapi/log.py
@@ -30,9 +30,9 @@
 """Logging system"""
 
 import logging
-import sys
 from logging.handlers import RotatingFileHandler
 from logging.handlers import TimedRotatingFileHandler
+import sys
 
 LOGGER = logging.getLogger(__name__)
 
