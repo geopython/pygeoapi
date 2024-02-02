d63ec9191077adc264430c179f346481cc3fb5b5
Peter Garnæs <pga@dmi.dk>
2024-02-02 15:24:54 +0100

    EDR queries without instance id works
    Django URL's without instance IDs called a method requiring instance
    IDs, resulting in a crash.
    
    Instance ID defaults to None, which is also standard for the provider
    that ultimately will be called.
    
    Regression test added

--- tests/test_django.py
+++ tests/test_django.py
@@ -6,8 +6,14 @@ from unittest import mock
 import django
 from django.test import Client
 
+from .util import get_test_file_path
 
-@mock.patch.dict(os.environ, {"DJANGO_SETTINGS_MODULE": "django_.settings"})
+
+@mock.patch.dict(os.environ, {
+    "DJANGO_SETTINGS_MODULE": "django_.settings",
+    "PYGEOAPI_CONFIG": get_test_file_path('pygeoapi-test-config.yml'),
+    "PYGEOAPI_OPENAPI": get_test_file_path('pygeoapi-test-openapi.yml')
+})
 @mock.patch.object(sys, "path", sys.path + ["./pygeoapi"])
 def test_django_landing_page_loads():
     django.setup()
@@ -16,3 +22,17 @@ def test_django_landing_page_loads():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json()["title"] == "pygeoapi default instance"
+
+
+def test_django_edr_without_instance_id():
+    django.setup()
+
+    edr_position_query = ("/collections/icoads-sst/position?coords="
+                          "POINT(12.779895 55.783523)&f=json")
+    response = Client(SERVER_NAME="localhost").get(edr_position_query)
+
+    assert response.status_code == HTTPStatus.OK
+    # Validate CoverageJSON is returned
+    response_json = response.json()
+    assert response_json["type"] == "Coverage"
+    assert response_json["domain"]["domainType"] == "Grid"
