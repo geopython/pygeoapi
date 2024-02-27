6a3f43f6b871ed04a18180db2bc140fc8e30b6d4
Francesco Bartoli <xbartolone@gmail.com>
2024-02-27 23:09:21 +0000

    Fix missing conformance class

--- pygeoapi/api_/feature.py
+++ pygeoapi/api_/feature.py
@@ -50,6 +50,7 @@ conformance_classes = [
     'http://www.opengis.net/spec/ogcapi-features-2/1.0/conf/crs',
     'http://www.opengis.net/spec/ogcapi-features-3/1.0/conf/queryables',
     'http://www.opengis.net/spec/ogcapi-features-3/1.0/conf/queryables-query-parameters',  # noqa
+    'http://www.opengis.net/spec/ogcapi-features-4/1.0/conf/create-replace-delete',  # noqa
     'http://www.opengis.net/spec/ogcapi-features-5/1.0/conf/schemas',
     'http://www.opengis.net/spec/ogcapi-features-5/1.0/req/core-roles-features' # noqa
 ]
