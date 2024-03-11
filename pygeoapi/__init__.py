ab4fe09192461b6df6e594a1cceb16d7488a9805
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2024-03-11 13:01:33 +0200

    update release version

--- pygeoapi/__init__.py
+++ pygeoapi/__init__.py
@@ -5,6 +5,7 @@
 #
 # Copyright (c) 2021 Tom Kralidis
 # Copyright (c) 2023 Ricardo Garcia Silva
+# Copyright (c) 2024 Angelos Tzotsos
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
@@ -29,7 +30,7 @@
 #
 # =================================================================
 
-__version__ = '0.16.dev0'
+__version__ = '0.16.0'
 
 import click
 try:
