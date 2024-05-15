dcabbb956f31daac1742fd4964758607ace23912
Moritz Langer <31667222+Moritz-Langer@users.noreply.github.com>
2024-05-15 03:13:49 +0200

    fixed issue 1652 by removing the staticmethod decorators infront of function defintion (#1653)

--- pygeoapi/api/itemtypes.py
+++ pygeoapi/api/itemtypes.py
@@ -1182,7 +1182,6 @@ def get_collection_item(api: API, request: APIRequest,
     return headers, HTTPStatus.OK, to_json(content, api.pretty_print)
 
 
-@staticmethod
 def create_crs_transform_spec(
         config: dict, query_crs_uri: Optional[str] = None) -> Union[None, CrsTransformSpec]:  # noqa
     """
@@ -1245,7 +1244,6 @@ def create_crs_transform_spec(
         return None
 
 
-@staticmethod
 def set_content_crs_header(
         headers: dict, config: dict, query_crs_uri: Optional[str] = None):
     """
