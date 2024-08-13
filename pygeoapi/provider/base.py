2a131c51311ad8c00bad475675abcb5cb3709fdc
Tom Kralidis <tomkralidis@gmail.com>
2024-08-13 11:12:44 -0400

    update docstrings for base provider fields functionality (#1783)
    * update docstrings for base provider fields functionality
    
    * fix flake8

--- pygeoapi/provider/base.py
+++ pygeoapi/provider/base.py
@@ -85,7 +85,8 @@ class BaseProvider:
         """
         Get provider field information (names, types)
 
-        Example response: {'field1': 'string', 'field2': 'number'}}
+        Example response:
+            {'field1': {'type': 'string'}, 'field2': {'type': 'number'}}
 
         :returns: dict of field names and their associated JSON Schema types
         """
@@ -97,7 +98,8 @@ class BaseProvider:
         """
         Store provider field information (names, types)
 
-        Example response: {'field1': {'type': 'string'}}
+        Example response:
+            {'field1': {'type': 'string'}, 'field2': {'type': 'number'}}
 
         :returns: dict of dicts (field names and their
                   associated JSON Schema definitions)
