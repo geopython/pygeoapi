9f2e209b67be9732d30161610a2268099c1b5ec7
Bernhard Mallinger <bernhard.mallinger@eox.at>
2024-08-19 12:52:22 +0200

    Add extra params to properties
    Oracle provider still needs to be adapted to this change

--- pygeoapi/api/itemtypes.py
+++ pygeoapi/api/itemtypes.py
@@ -226,8 +226,6 @@ def get_collection_items(
                            'properties', 'skipGeometry', 'q',
                            'filter', 'filter-lang', 'filter-crs']
 
-    extra_params = {}
-
     collections = filter_dict_by_key_value(api.config['resources'],
                                            'type', 'collection')
 
@@ -390,11 +388,11 @@ def get_collection_items(
     for k, v in request.params.items():
         if k not in reserved_fieldnames:
             if k in list(p.fields.keys()):
-                LOGGER.debug(f'Adding property filter {k}={v}')
-                properties.append((k, v))
+                LOGGER.debug(f'Adding custom property filter {k}={v}')
             else:
-                LOGGER.debug(f'Adding extra filter {k}={v}')
-                extra_params[str(k).lower()] = v
+                LOGGER.debug('Adding property filter {k}={v}')
+
+            properties.append((k, v))
 
     LOGGER.debug('processing sort parameter')
     val = request.params.get('sortby')
@@ -485,7 +483,6 @@ def get_collection_items(
     LOGGER.debug(f'datetime: {datetime_}')
     LOGGER.debug(f'properties: {properties}')
     LOGGER.debug(f'select properties: {select_properties}')
-    LOGGER.debug(f'extra_params: {extra_params}')
     LOGGER.debug(f'skipGeometry: {skip_geometry}')
     LOGGER.debug(f'language: {prv_locale}')
     LOGGER.debug(f'q: {q}')
@@ -501,8 +498,7 @@ def get_collection_items(
                           sortby=sortby, skip_geometry=skip_geometry,
                           select_properties=select_properties,
                           crs_transform_spec=crs_transform_spec,
-                          q=q, language=prv_locale, filterq=filter_,
-                          extra_params=extra_params)
+                          q=q, language=prv_locale, filterq=filter_)
     except ProviderGenericError as err:
         return api.get_exception(
             err.http_status_code, headers, request.format,
