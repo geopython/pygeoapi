d3c6726b9bbfd7e093eb4357e4532947a555a86f
Piotr Zaborowski <pzb@bixbite.eu>
2024-09-19 12:50:22 +0200

    whitespaces

--- pygeoapi/provider/xarray_.py
+++ pygeoapi/provider/xarray_.py
@@ -250,12 +250,12 @@ class XarrayProvider(BaseProvider):
         LOGGER.debug('Creating CoverageJSON domain')
         startx, starty, stopx, stopy = metadata['bbox']
         mint, maxt = metadata['time']
-        
+
         selected_fields = {
             key: value for key, value in self.fields.items()
             if key in fields
         }
-        
+
         cj = {
             'type': 'Coverage',
             'domain': {
