630c108ca19fc0f821c6e4a5b55fc17e8f1a89aa
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2025-01-15 17:53:18 -0500

    Use **kwargs
    cc https://github.com/geopython/pygeoapi/pull/1899

--- pygeoapi/process/echo.py
+++ pygeoapi/process/echo.py
@@ -115,7 +115,7 @@ class EchoProcessor(BaseProcessor):
 
         super().__init__(processor_def, PROCESS_METADATA)
 
-    def execute(self, data, outputs=None):
+    def execute(self, data, **kwargs):
 
         mimetype = 'application/json'
 
