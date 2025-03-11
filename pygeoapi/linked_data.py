9d6095083c31323406ad813d7635d539c20ff25c
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2025-03-10 20:54:17 -0400

    Support GeoJSON-LD templating (#1927)
    * Support JSON-LD template for items
    
    * Create `item_list.jsonld` template
    
    Create `item_list.jsonld` template to backport current /items json-ld content offerings
    
    * Reduce number of times templated geojsonld is serialized
    
    * Add test for linked_data.py
    
    * Use json serializer in test
    
    * Run test in CI
    
    * Add test for single item
    
    * Use single quotes
    
    * Fix flake8
    
    * Use collection level template for GeoJSON-LD
    
    * Un-nest JSON-LD context from resource config
    
    * Respond to PR feedback
    
    * Update test_linked_data.py
    
    * Revert "Un-nest JSON-LD context from resource config"
    
    This reverts commit 5b371b26166c320174459627625e57cf497aaab6.
    
    * Update documentation

--- pygeoapi/linked_data.py
+++ pygeoapi/linked_data.py
@@ -31,11 +31,10 @@
 Returns content as linked data representations
 """
 
-import json
 import logging
 from typing import Callable
 
-from pygeoapi.util import is_url, render_j2_template
+from pygeoapi.util import is_url, render_j2_template, url_join
 from pygeoapi import l10n
 from shapely.geometry import shape
 from shapely.ops import unary_union
@@ -189,30 +188,22 @@ def geojson2jsonld(cls, data: dict, dataset: str,
     :returns: string of rendered JSON (GeoJSON-LD)
     """
 
-    LOGGER.debug('Fetching context and template from resource configuration')
-    jsonld = cls.config['resources'][dataset].get('linked-data', {})
-    ds_url = f"{cls.get_collections_url()}/{dataset}"
-
-    context = jsonld.get('context', []).copy()
-    template = jsonld.get('item_template', None)
+    LOGGER.debug('Fetching context from resource configuration')
+    context = cls.config['resources'][dataset].get('context', []).copy()
+    templates = cls.get_dataset_templates(dataset)
 
     defaultVocabulary = {
         'schema': 'https://schema.org/',
+        'gsp': 'http://www.opengis.net/ont/geosparql#',
         'type': '@type'
     }
 
     if identifier:
-        # Single jsonld
-        defaultVocabulary.update({
-            'gsp': 'http://www.opengis.net/ont/geosparql#'
-        })
-
         # Expand properties block
         data.update(data.pop('properties'))
 
         # Include multiple geometry encodings
         if (data.get('geometry') is not None):
-            data['type'] = 'schema:Place'
             jsonldify_geometry(data)
 
         data['@id'] = identifier
@@ -224,6 +215,7 @@ def geojson2jsonld(cls, data: dict, dataset: str,
             'FeatureCollection': 'schema:itemList'
         })
 
+        ds_url = url_join(cls.get_collections_url(), dataset)
         data['@id'] = ds_url
 
         for i, feature in enumerate(data['features']):
@@ -233,9 +225,15 @@ def geojson2jsonld(cls, data: dict, dataset: str,
             if not is_url(str(identifier_)):
                 identifier_ = f"{ds_url}/items/{feature['id']}"  # noqa
 
+            # Include multiple geometry encodings
+            if feature.get('geometry') is not None:
+                jsonldify_geometry(feature)
+
             data['features'][i] = {
                 '@id': identifier_,
-                'type': 'schema:Place'
+                'type': 'schema:Place',
+                **feature.pop('properties'),
+                **feature
             }
 
     if data.get('timeStamp', False):
@@ -248,17 +246,21 @@ def geojson2jsonld(cls, data: dict, dataset: str,
         **data
     }
 
-    if None in (template, identifier):
-        return ldjsonData
-    else:
-        # Render jsonld template for single item with template configured
-        LOGGER.debug(f'Rendering JSON-LD template: {template}')
+    if identifier:
+        # Render jsonld template for single item
+        LOGGER.debug('Rendering JSON-LD item template')
         content = render_j2_template(
-            cls.config, cls.config['server']['templates'],
-            template, ldjsonData)
+            cls.tpl_config, templates,
+            'collections/items/item.jsonld', ldjsonData)
 
-        ldjsonData = json.loads(content)
-        return ldjsonData
+    else:
+        # Render jsonld template for /items
+        LOGGER.debug('Rendering JSON-LD items template')
+        content = render_j2_template(
+            cls.tpl_config, templates,
+            'collections/items/index.jsonld', ldjsonData)
+
+    return content
 
 
 def jsonldify_geometry(feature: dict) -> None:
@@ -270,6 +272,8 @@ def jsonldify_geometry(feature: dict) -> None:
 
     :returns: None
     """
+
+    feature['type'] = 'schema:Place'
 
     geo = feature.get('geometry')
     geom = shape(geo)
@@ -287,7 +291,11 @@ def jsonldify_geometry(feature: dict) -> None:
     }
 
     # Schema geometry
-    feature['schema:geo'] = geom2schemageo(geom)
+    try:
+        feature['schema:geo'] = geom2schemageo(geom)
+    except AttributeError:
+        msg = f'Unable to parse schema geometry for {feature["id"]}'
+        LOGGER.warning(msg)
 
 
 def geom2schemageo(geom: shape) -> dict:
