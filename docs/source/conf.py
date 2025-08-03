5c6d292981043f6e9441ee12a212963c5f4c27f2
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2025-08-03 18:48:41 +0300

    back to dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,7 +112,7 @@ today_fmt = '%Y-%m-%d'
 # built documents.
 #
 # The short X.Y version.
-version = '0.21.0'
+version = '0.22.dev0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
