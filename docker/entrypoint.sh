4e2175906d0a52cf950af1babd6fbe79345aa544
cmotadev <cmota.dev@gmail.com>
2025-08-14 19:42:27 -0300

    Allowed to modify PYGEOAPI_CONFIG and PYGEOAPI_OPENAPI envs on container start.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -37,8 +37,13 @@ echo "START /entrypoint.sh"
 set +e
 
 export PYGEOAPI_HOME=/pygeoapi
-export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
-export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
+
+if [[ -z "$PYGEOAPI_CONFIG" ]]; then
+	export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
+fi
+if [[ -z "$PYGEOAPI_OPENAPI" ]]; then
+	export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
+fi
 
 # gunicorn env settings with defaults
 SCRIPT_NAME=${SCRIPT_NAME:=/}
@@ -61,6 +66,8 @@ function error() {
 
 # Workdir
 cd ${PYGEOAPI_HOME}
+
+echo "Default config in ${PYGEOAPI_CONFIG}"
 
 echo "Trying to generate openapi.yml"
 pygeoapi openapi generate ${PYGEOAPI_CONFIG} --output-file ${PYGEOAPI_OPENAPI}
