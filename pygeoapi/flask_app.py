56ff1082cda85bb4757fcb0d42d47dbdd99593b8
Csenongmin <csm010311@naver.com>
2026-01-19 14:11:14 +0900

    GET, DELETE for layer endpoint, add DEBUG in config, fix openapi schema for ThematicLayer part

--- pygeoapi/flask_app.py
+++ pygeoapi/flask_app.py
@@ -257,47 +257,7 @@ def collections(collection_id: str | None = None):
                               'MethodNotAllowed', 'Method not allowed')
 
 
-@BLUEPRINT.route('/collections/<path:collection_id>/items/<path:item_id>/layers', methods=['GET', 'POST'])
-@BLUEPRINT.route('/collections/<path:collection_id>/items/<path:item_id>/layers/<path:layer_id>', methods=['GET', 'DELETE'])
-def collection_items_layers(collection_id, item_id, layer_id=None):
-    """
-    OGC API collections items endpoint
-    
-    :param collection_id: collection identifier
-    :param item_id: item identifier
-    
-    :reaturns: HTTP response
-    """
-    if layer_id is None:
-        if request.method == 'GET':  #list metadata of thematic layers.
-            return execute_from_flask(
-                indoorgml.get_collection_item_layers, request, 
-                collection_id, 
-                item_id
-            )
-        elif request.method == 'POST':  # create new thematic layer
-            return execute_from_flask(
-                indoorgml.manage_collection_item_layer, request, 
-                'create', 
-                collection_id, 
-                item_id
-            )
-    else:
-        if request.method == 'GET':  # read data of specific thematic layer
-            return execute_from_flask(
-                indoorgml.get_collection_item_layer, request, 
-                collection_id,
-                item_id,
-                layer_id
-            )
-        elif request.method == 'DELETE':  #delete specific thematic layer
-            return execute_from_flask(
-                indoorgml.manage_collection_item_layer, request,
-                'delete',
-                collection_id,
-                item_id,
-                layer_id
-            )
+
 
 @BLUEPRINT.route('/collections/<path:collection_id>/schema')
 def collection_schema(collection_id: str | None = None):
@@ -394,6 +354,47 @@ def collection_items(collection_id: str, item_id: str | None = None):
         return execute_from_flask(itemtypes_api.get_collection_item, request,
                                   collection_id, item_id)
 
+@BLUEPRINT.route('/collections/<path:collection_id>/items/<path:item_id>/layers', methods=['GET', 'POST'])
+@BLUEPRINT.route('/collections/<path:collection_id>/items/<path:item_id>/layers/<path:layer_id>', methods=['GET', 'DELETE'])
+def collection_items_layers(collection_id, item_id, layer_id=None):
+    """
+    OGC API collections items endpoint
+    
+    :param collection_id: collection identifier
+    :param item_id: item identifier
+    
+    :reaturns: HTTP response
+    """
+    if layer_id is None:
+        if request.method == 'GET':  #list metadata of thematic layers.
+            return execute_from_flask(
+                indoorgml.get_collection_item_layers, request, 
+                collection_id, 
+                item_id
+            )
+        elif request.method == 'POST':  # create new thematic layer
+            return execute_from_flask(
+                indoorgml.manage_collection_item_layer, request, 
+                'create', 
+                collection_id, 
+                item_id
+            )
+    else:
+        if request.method == 'GET':  # read data of specific thematic layer
+            return execute_from_flask(
+                indoorgml.get_collection_item_layer, request, 
+                collection_id,
+                item_id,
+                layer_id
+            )
+        elif request.method == 'DELETE':  #delete specific thematic layer
+            return execute_from_flask(
+                indoorgml.manage_collection_item_layer, request,
+                'delete',
+                collection_id,
+                item_id,
+                layer_id
+            )
 
 @BLUEPRINT.route('/collections/<path:collection_id>/coverage')
 def collection_coverage(collection_id: str):
