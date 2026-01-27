4c8b6446a30507e0ba25c846cae1a55ccce39773
Matthew Dear <matthew.dear@os.uk>
2026-01-27 08:51:59 +0000

    fix: move count to base provider
    This work has been done to move count to the base provider. While doing this work I also added a debug log message to state when the count had been disabled in the SQL provider.
    
    Also, I removed some tests that were no longer needed after the introduction of the str2bool function, when getting the count value from the configuration file.

--- pygeoapi/provider/sql.py
+++ pygeoapi/provider/sql.py
@@ -127,7 +127,6 @@ class GenericSQLProvider(BaseProvider):
         self.id_field = provider_def['id_field']
         self.geom = provider_def.get('geom_field', 'geom')
         self.driver_name = driver_name
-        self.count = str(provider_def.get('count', 'true')).lower() == 'true'
 
         LOGGER.debug(f'Name: {self.name}')
         LOGGER.debug(f'Table: {self.table}')
@@ -224,6 +223,8 @@ class GenericSQLProvider(BaseProvider):
                 matched = results.count()
                 response['numberMatched'] = matched
                 LOGGER.debug(f'Found {matched} result(s)')
+            else:
+                LOGGER.debug('Count disabled')
 
             if resulttype == 'hits' or not results:
                 return response
