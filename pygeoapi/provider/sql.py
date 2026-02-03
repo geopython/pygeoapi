7e47b026440bb5573acda17299e27785ddb32d2d
Matthew Dear <160152917+mdearos@users.noreply.github.com>
2026-02-03 11:18:06 +0000

    fix: add fix for slow full count in postgresql (#2174)
    * feat: implement optional count for postgresql provider
    
    This work has been done to allow the result count to be enabled or disabled for the PostgreSQL provider. By disabling the count you can get improved performance on large datasets but on smaller datasets this is unlikely to have any affect.
    
    * fix: move count to base provider
    
    This work has been done to move count to the base provider. While doing this work I also added a debug log message to state when the count had been disabled in the SQL provider.
    
    Also, I removed some tests that were no longer needed after the introduction of the str2bool function, when getting the count value from the configuration file.
    
    * fix: convert string true to boolean

--- pygeoapi/provider/sql.py
+++ pygeoapi/provider/sql.py
@@ -91,7 +91,6 @@ from pygeoapi.provider.base import (
     ProviderQueryError,
     ProviderItemNotFoundError
 )
-from pygeoapi.util import str2bool
 
 LOGGER = logging.getLogger(__name__)
 
@@ -128,7 +127,6 @@ class GenericSQLProvider(BaseProvider):
         self.id_field = provider_def['id_field']
         self.geom = provider_def.get('geom_field', 'geom')
         self.driver_name = driver_name
-        self.count = str2bool(provider_def.get('count', True))
 
         LOGGER.debug(f'Name: {self.name}')
         LOGGER.debug(f'Table: {self.table}')
@@ -214,17 +212,19 @@ class GenericSQLProvider(BaseProvider):
                 .options(selected_properties)
             )
 
-            matched = results.count()
-
-            LOGGER.debug(f'Found {matched} result(s)')
-
             LOGGER.debug('Preparing response')
             response = {
                 'type': 'FeatureCollection',
                 'features': [],
-                'numberMatched': matched,
                 'numberReturned': 0
             }
+
+            if self.count or resulttype == 'hits':
+                matched = results.count()
+                response['numberMatched'] = matched
+                LOGGER.debug(f'Found {matched} result(s)')
+            else:
+                LOGGER.debug('Count disabled')
 
             if resulttype == 'hits' or not results:
                 return response
