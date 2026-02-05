0dd01d66621d1b46dc0c377bbb57b940bf29e679
Csenongmin <csm010311@naver.com>
2026-02-05 19:49:15 +0900

    02/05 add sample collection

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 # 1. Define the Environment Variables
 export PYTHONPATH=$(pwd)
 export PYGEOAPI_CONFIG=$(pwd)/pygeoapi-config.yml
-export PYGEOAPI_OPENAPI=$(pwd)/local.openapi.yml
+export PYGEOAPI_OPENAPI=$(pwd)/openAPI/IndoorGML_openAPI.yml
 
 # 2. Generate the OpenAPI Document
 # This creates the 'local.openapi.yml' file from your config. 
 # We run this every time to ensure your API changes (like new collections) appear in Swagger.
 echo "Regenerating OpenAPI configuration..."
-pygeoapi openapi generate "$PYGEOAPI_CONFIG" --output-file "$PYGEOAPI_OPENAPI"
+# pygeoapi openapi generate "$PYGEOAPI_CONFIG" --output-file "$PYGEOAPI_OPENAPI"
 
 # 3. Start the Server
 echo "Starting pygeoapi server..."
