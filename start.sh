9aca77c2735fa4a1c3e6a33a2942cd4358f570b8
stem <stem@stems-MacBook-Pro.local>
2026-02-06 18:15:12 +0900

    edit the landing page

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # 1. Define the Environment Variables
 export PYTHONPATH=$(pwd)
 export PYGEOAPI_CONFIG=$(pwd)/pygeoapi-config.yml
-export PYGEOAPI_OPENAPI=$(pwd)/indoorgmlapi_bundled.yaml
+export PYGEOAPI_OPENAPI=$(pwd)/openAPI/indoorgmlapi_bundled.yaml
 
 # 2. Generate the OpenAPI Document
 # This creates the 'local.openapi.yml' file from your config. 
