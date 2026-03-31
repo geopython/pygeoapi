c80bc697aabac18b437746698ed1586454ae7b72
Benjamin Webb <benjamin.miller.webb@gmail.com>
2026-03-31 09:39:45 -0400

    Fix flake8

--- pygeoapi/util.py
+++ pygeoapi/util.py
@@ -265,10 +265,9 @@ def to_json(dict_: dict, pretty: bool = False) -> str:
     json_dump = json.dumps(dict_, default=json_serial, indent=indent,
                            separators=(',', ':'))
 
-    LOGGER.debug('Removing < and >')
+    LOGGER.debug('Escaping < and >')
     json_dump = json_dump.replace('<', '&lt;')
     json_dump = json_dump.replace('>', '&gt;')
-
 
     return json_dump
 
