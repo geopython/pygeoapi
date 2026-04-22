a0dcc61664c770fe7b407ddc6daf8d7f93d4dc32
Tom Kralidis <tomkralidis@gmail.com>
2026-04-21 22:11:33 -0400

    update release version 0.23.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,7 +112,7 @@ today_fmt = '%Y-%m-%d'
 # built documents.
 #
 # The short X.Y version.
-version = '0.23.2'
+version = '0.23.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
